9bef6a446c92a0f6fa08bb93b67512d8398b1227
Joshua van der Burg <joshuaburg2004@hotmail.nl>
2026-03-02 14:47:05 +0100

    Alien works!

--- SpaceDefence/Alien.cs
+++ SpaceDefence/Alien.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -9,6 +10,10 @@ namespace SpaceDefence
         private CircleCollider _circleCollider;
         private Texture2D _texture;
         private float playerClearance = 100;
+        private int version = 0;
+        private float accelerationRate = 50f;
+        private float[] maxSpeed = [0f, 50f, 100f, 150f, 200f, 250f];
+        private Vector2 velocity = Vector2.Zero;
 
         public Alien() 
         {
@@ -27,6 +32,8 @@ namespace SpaceDefence
         public override void OnCollision(GameObject other)
         {
             RandomMove();
+            // Set the new max speed to the new version. Never go faster than player
+            version = Math.Clamp(version + 1, 0, 5);
             base.OnCollision(other);
         }
 
@@ -46,6 +53,40 @@ namespace SpaceDefence
             base.Draw(gameTime, spriteBatch);
         }
 
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
 
+            GameManager gm = GameManager.GetGameManager();
+            Vector2 playerPosition = gm.Player.GetPosition().Center.ToVector2();
+            Vector2 alienPosition = _circleCollider.Center;
+
+            // Calculate direction to the player
+            Vector2 direction = playerPosition - alienPosition;
+            if (direction.Length() > 0)
+                direction.Normalize();
+
+            // Accelerate toward the player
+            velocity += direction * accelerationRate * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Clamp velocity to max speed for the current version
+            float currentMaxSpeed = maxSpeed[version];
+            if (velocity.Length() > currentMaxSpeed)
+            {
+                velocity.Normalize();
+                velocity *= currentMaxSpeed;
+            }
+
+            Vector2 newPosition = _circleCollider.Center;
+            newPosition += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //Ensure Ship stays on screen
+            newPosition.X = MathHelper.Clamp(newPosition.X, 0, GameManager.GetGameManager().Game.GraphicsDevice.Viewport.Width);
+            newPosition.Y = MathHelper.Clamp(newPosition.Y, 0, GameManager.GetGameManager().Game.GraphicsDevice.Viewport.Height);
+            // New position
+
+            // Update position
+            _circleCollider.Center = newPosition;
+        }
     }
 }
