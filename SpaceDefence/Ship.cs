16b49d64338200665fd55cdf7c6237b1f57a4467
Joshua van der Burg <joshuaburg2004@hotmail.nl>
2026-03-05 09:55:51 +0100

    Drawn border works now(?)

--- SpaceDefence/Ship.cs
+++ SpaceDefence/Ship.cs
@@ -16,6 +16,7 @@ namespace SpaceDefence
         private float buffTimer = 100;
         private float buffDuration = 10f;
         private RectangleCollider _rectangleCollider;
+        private Rectangle drawRectangle;
         private Point target;
         private Vector2 velocity;
         private float accelerationRate = 30f;
@@ -39,7 +40,12 @@ namespace SpaceDefence
             base_turret = content.Load<Texture2D>("base_turret");
             laser_turret = content.Load<Texture2D>("laser_turret");
             _rectangleCollider.shape.Size = ship_body.Bounds.Size;
-            _rectangleCollider.shape.Location -= new Point(ship_body.Width/2, ship_body.Height/2);
+            _rectangleCollider.shape.Location -= new Point(ship_body.Width / 2, ship_body.Height / 2);
+            drawRectangle = new Rectangle(
+                _rectangleCollider.shape.Center,
+                ship_body.Bounds.Size
+            );
+
             base.Load(content);
         }
 
@@ -108,36 +114,41 @@ namespace SpaceDefence
             // Update the Buff timer
             if (buffTimer > 0)
                 buffTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             // Apply velocity
             Vector2 newPosition = _rectangleCollider.shape.Location.ToVector2();
             newPosition += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            //Ensure Ship stays on screen
+            // Ensure Ship stays on screen
             newPosition.X = MathHelper.Clamp(newPosition.X, 0, GameManager.GetGameManager().Game.GraphicsDevice.Viewport.Width - _rectangleCollider.shape.Width);
             newPosition.Y = MathHelper.Clamp(newPosition.Y, 0, GameManager.GetGameManager().Game.GraphicsDevice.Viewport.Height - _rectangleCollider.shape.Height);
-            // New position
+            // Update collider position
             _rectangleCollider.shape.Location = newPosition.ToPoint();
+
+            // Update the draw rectangle to match the collider's position
+            drawRectangle.Location = _rectangleCollider.shape.Center;
 
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            //void SpriteBatch.Draw(, float rotation, Vector2 origin, SpriteEffects effects, float layerDepth)
-            spriteBatch.Draw(ship_body, _rectangleCollider.shape, null, Color.White, angle, ship_body.Bounds.Size.ToVector2() / 2f, SpriteEffects.None, 0);
+            spriteBatch.Draw(ship_body, drawRectangle, null, Color.White, angle, ship_body.Bounds.Size.ToVector2() / 2f, SpriteEffects.None, 0);
+
             float aimAngle = LinePieceCollider.GetAngle(LinePieceCollider.GetDirection(GetPosition().Center, target));
             if (buffTimer <= 0)
             {
                 Rectangle turretLocation = base_turret.Bounds;
-                turretLocation.Location = new Point(_rectangleCollider.shape.Left, _rectangleCollider.shape.Top);
+                turretLocation.Location = new Point(drawRectangle.Left, drawRectangle.Top);
                 spriteBatch.Draw(base_turret, turretLocation, null, Color.White, aimAngle, turretLocation.Size.ToVector2() / 2f, SpriteEffects.None, 0);
             }
             else
             {
                 Rectangle turretLocation = laser_turret.Bounds;
-                turretLocation.Location = new Point(_rectangleCollider.shape.Left, _rectangleCollider.shape.Top);
+                turretLocation.Location = new Point(drawRectangle.Left, drawRectangle.Top);
                 spriteBatch.Draw(laser_turret, turretLocation, null, Color.White, aimAngle, turretLocation.Size.ToVector2() / 2f, SpriteEffects.None, 0);
             }
+
             base.Draw(gameTime, spriteBatch);
         }
 
