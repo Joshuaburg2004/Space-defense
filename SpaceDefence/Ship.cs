ef8435970a1d558843b35c251694c669a79720e3
joshuaburg2004 <joshuaburg2004@hotmail.nl>
2026-03-12 15:03:51 +0100

    Keep ship in map area

--- SpaceDefence/Ship.cs
+++ SpaceDefence/Ship.cs
@@ -1,10 +1,9 @@
-﻿using SpaceDefence.Collision;
+﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
-using System;
+using SpaceDefence.Collision;
 
 namespace SpaceDefence
 {
@@ -40,20 +39,19 @@ namespace SpaceDefence
             base_turret = content.Load<Texture2D>("base_turret");
             laser_turret = content.Load<Texture2D>("laser_turret");
             _rectangleCollider.shape.Size = ship_body.Bounds.Size;
-            _rectangleCollider.shape.Location -= new Point(ship_body.Width / 2, ship_body.Height / 2);
-            drawRectangle = new Rectangle(
-                _rectangleCollider.shape.Center,
-                ship_body.Bounds.Size
+            _rectangleCollider.shape.Location -= new Point(
+                ship_body.Width / 2,
+                ship_body.Height / 2
             );
+            drawRectangle = new Rectangle(_rectangleCollider.shape.Center, ship_body.Bounds.Size);
 
             base.Load(content);
         }
 
-
         public override void HandleInput(InputManager inputManager)
         {
             base.HandleInput(inputManager);
-            
+
             GameManager gm = GameManager.GetGameManager();
             if (inputManager.IsKeyPress(Keys.Escape))
             {
@@ -64,28 +62,34 @@ namespace SpaceDefence
             }
             if (gm.gameState == GameManager.GameState.Playing)
             {
-                target = gm.WorldCamera.ScreenToWorld(inputManager.CurrentMouseState.Position, gm.Game.GraphicsDevice.Viewport.Width, gm.Game.GraphicsDevice.Viewport.Height);
+                target = gm.WorldCamera.ScreenToWorld(
+                    inputManager.CurrentMouseState.Position,
+                    gm.Game.GraphicsDevice.Viewport.Width,
+                    gm.Game.GraphicsDevice.Viewport.Height
+                );
                 // Check W, A, S and D, adjust momentum accordingly
                 Vector2 acceleration = new Vector2();
-                if(inputManager.IsKeyDown(Keys.W))
+                if (inputManager.IsKeyDown(Keys.W))
                 {
                     acceleration.Y -= 1;
                 }
-                if(inputManager.IsKeyDown(Keys.S))
+                if (inputManager.IsKeyDown(Keys.S))
                 {
                     acceleration.Y += 1;
                 }
-                if(inputManager.IsKeyDown(Keys.A))
+                if (inputManager.IsKeyDown(Keys.A))
                 {
                     acceleration.X -= 1;
                 }
-                if(inputManager.IsKeyDown(Keys.D))
+                if (inputManager.IsKeyDown(Keys.D))
                 {
                     acceleration.X += 1;
                 }
-                if (acceleration != Vector2.Zero) { 
+                if (acceleration != Vector2.Zero)
+                {
                     acceleration.Normalize();
-                    float targetAngle = (float)Math.Atan2(acceleration.Y, acceleration.X) + (float)Math.PI / 2.0f;
+                    float targetAngle =
+                        (float)Math.Atan2(acceleration.Y, acceleration.X) + (float)Math.PI / 2.0f;
                     float rotationSpeed = 0.15f;
                     float angleDifference = MathHelper.WrapAngle(targetAngle - angle);
                     angle += angleDifference * rotationSpeed;
@@ -96,17 +100,30 @@ namespace SpaceDefence
                 {
                     velocity = Vector2.Normalize(velocity) * maxSpeed;
                 }
-                if(inputManager.LeftMousePress())
+                if (inputManager.LeftMousePress())
                 {
-                    Vector2 aimDirection = LinePieceCollider.GetDirection(GetPosition().Center, target);
-                    Vector2 turretExit = drawRectangle.Location.ToVector2() + aimDirection * base_turret.Height / 2f;
+                    Vector2 aimDirection = LinePieceCollider.GetDirection(
+                        GetPosition().Center,
+                        target
+                    );
+                    Vector2 turretExit =
+                        drawRectangle.Location.ToVector2() + aimDirection * base_turret.Height / 2f;
                     if (buffTimer <= 0)
                     {
-                        GameManager.GetGameManager().AddGameObject(new Bullet(turretExit, aimDirection, 350));
+                        GameManager
+                            .GetGameManager()
+                            .AddGameObject(new Bullet(turretExit, aimDirection, 350));
                     }
                     else
                     {
-                        GameManager.GetGameManager().AddGameObject(new Laser(new LinePieceCollider(turretExit, target.ToVector2()),400));
+                        GameManager
+                            .GetGameManager()
+                            .AddGameObject(
+                                new Laser(
+                                    new LinePieceCollider(turretExit, target.ToVector2()),
+                                    400
+                                )
+                            );
                     }
                 }
             }
@@ -121,7 +138,17 @@ namespace SpaceDefence
             // Apply velocity
             Vector2 newPosition = _rectangleCollider.shape.Location.ToVector2();
             newPosition += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
+            // Ensure Ship stays on screen
+            newPosition.X = MathHelper.Clamp(
+                newPosition.X,
+                0,
+                GameManager.GetGameManager().map.Width - _rectangleCollider.shape.Width
+            );
+            newPosition.Y = MathHelper.Clamp(
+                newPosition.Y,
+                0,
+                GameManager.GetGameManager().map.Height - _rectangleCollider.shape.Height
+            );
             // Update collider position
             _rectangleCollider.shape.Location = newPosition.ToPoint();
             GameManager.GetGameManager().WorldCamera.AdjustPosition(newPosition);
@@ -134,20 +161,49 @@ namespace SpaceDefence
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(ship_body, drawRectangle, null, Color.White, angle, ship_body.Bounds.Size.ToVector2() / 2f, SpriteEffects.None, 0);
+            spriteBatch.Draw(
+                ship_body,
+                drawRectangle,
+                null,
+                Color.White,
+                angle,
+                ship_body.Bounds.Size.ToVector2() / 2f,
+                SpriteEffects.None,
+                0
+            );
 
-            float aimAngle = LinePieceCollider.GetAngle(LinePieceCollider.GetDirection(GetPosition().Center, target));
+            float aimAngle = LinePieceCollider.GetAngle(
+                LinePieceCollider.GetDirection(GetPosition().Center, target)
+            );
             if (buffTimer <= 0)
             {
                 Rectangle turretLocation = base_turret.Bounds;
                 turretLocation.Location = new Point(drawRectangle.Left, drawRectangle.Top);
-                spriteBatch.Draw(base_turret, turretLocation, null, Color.White, aimAngle, turretLocation.Size.ToVector2() / 2f, SpriteEffects.None, 0);
+                spriteBatch.Draw(
+                    base_turret,
+                    turretLocation,
+                    null,
+                    Color.White,
+                    aimAngle,
+                    turretLocation.Size.ToVector2() / 2f,
+                    SpriteEffects.None,
+                    0
+                );
             }
             else
             {
                 Rectangle turretLocation = laser_turret.Bounds;
                 turretLocation.Location = new Point(drawRectangle.Left, drawRectangle.Top);
-                spriteBatch.Draw(laser_turret, turretLocation, null, Color.White, aimAngle, turretLocation.Size.ToVector2() / 2f, SpriteEffects.None, 0);
+                spriteBatch.Draw(
+                    laser_turret,
+                    turretLocation,
+                    null,
+                    Color.White,
+                    aimAngle,
+                    turretLocation.Size.ToVector2() / 2f,
+                    SpriteEffects.None,
+                    0
+                );
             }
 
             base.Draw(gameTime, spriteBatch);
@@ -160,7 +216,6 @@ namespace SpaceDefence
                 GameManager.GetGameManager().gameState = GameManager.GameState.GameOver;
             }
         }
-
 
         public void Buff()
         {
